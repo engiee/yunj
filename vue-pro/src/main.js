a9eb479758462b4ae6aa5c1e3b10396e0c34098b
wangzhen <wangzhen@yunjiacloud.com>
2018-05-09 09:49:52 +0800

    vue-deno init

---
+++ vue-pro/src/main.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Layout from './components/layout.vue'
+import VueResource from 'vue-resource'
+import IndexPage from './pages/index.vue'
+import DetailPage from './pages/detail.vue'
+import OrderListPage from './pages/orderList.vue'
+import DetailAnaPage from './pages/detail/analysis.vue'
+import DetailCouPage from './pages/detail/count.vue'
+import DetailForPage from './pages/detail/forecast.vue'
+import DetailPubPage from './pages/detail/publish.vue'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+let router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			component: IndexPage
+		},
+		{
+			path: '/orderList',
+			component: OrderListPage
+		},
+		{
+			path: '/detail',
+			component: DetailPage,
+			redirect: '/detail/analysis',
+			children: [
+				{
+					path: 'analysis',
+					component: DetailAnaPage
+				},
+				{
+					path: 'count',
+					component: DetailCouPage
+				},
+				{
+					path: 'forecast',
+					component: DetailForPage
+				},
+				{
+					path: 'publish',
+					component: DetailPubPage
+				}
+			]
+		}
+	]
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
